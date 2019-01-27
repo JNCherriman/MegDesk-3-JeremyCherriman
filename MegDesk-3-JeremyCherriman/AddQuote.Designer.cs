db3872a00e5fa4c26903ac766ab046398ebff3af
Jeremy Cherriman <32113470+JNCherriman@users.noreply.github.com>
2019-01-27 07:37:54 -0500

    AddQuote form layout created and initial PARC set.

--- MegDesk-3-JeremyCherriman/AddQuote.Designer.cs
+++ MegDesk-3-JeremyCherriman/AddQuote.Designer.cs
@@ -28,21 +28,336 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.clientName = new System.Windows.Forms.TextBox();
+            this.deskWidth = new System.Windows.Forms.NumericUpDown();
+            this.deskDepth = new System.Windows.Forms.NumericUpDown();
+            this.numberOFDrawers = new System.Windows.Forms.NumericUpDown();
+            this.materialLaminate = new System.Windows.Forms.RadioButton();
+            this.materialOak = new System.Windows.Forms.RadioButton();
+            this.materialRosewood = new System.Windows.Forms.RadioButton();
+            this.materialVeneer = new System.Windows.Forms.RadioButton();
+            this.materialPine = new System.Windows.Forms.RadioButton();
+            this.rushNone = new System.Windows.Forms.RadioButton();
+            this.rush3Day = new System.Windows.Forms.RadioButton();
+            this.rush5Day = new System.Windows.Forms.RadioButton();
+            this.rush7Day = new System.Windows.Forms.RadioButton();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.customerName = new System.Windows.Forms.Label();
+            this.groupDeskDimensions = new System.Windows.Forms.GroupBox();
+            this.desktopDepth = new System.Windows.Forms.Label();
+            this.desktopWidth = new System.Windows.Forms.Label();
+            this.groupNumDrawers = new System.Windows.Forms.GroupBox();
+            this.numDrawers = new System.Windows.Forms.Label();
+            this.groupSurfaceMaterial = new System.Windows.Forms.GroupBox();
+            this.groupRush = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.deskWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deskDepth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOFDrawers)).BeginInit();
+            this.groupDeskDimensions.SuspendLayout();
+            this.groupNumDrawers.SuspendLayout();
+            this.groupSurfaceMaterial.SuspendLayout();
+            this.groupRush.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // clientName
+            // 
+            this.clientName.Location = new System.Drawing.Point(135, 24);
+            this.clientName.Name = "clientName";
+            this.clientName.Size = new System.Drawing.Size(200, 20);
+            this.clientName.TabIndex = 0;
+            // 
+            // deskWidth
+            // 
+            this.deskWidth.Location = new System.Drawing.Point(106, 30);
+            this.deskWidth.Name = "deskWidth";
+            this.deskWidth.Size = new System.Drawing.Size(120, 20);
+            this.deskWidth.TabIndex = 1;
+            // 
+            // deskDepth
+            // 
+            this.deskDepth.Location = new System.Drawing.Point(106, 76);
+            this.deskDepth.Name = "deskDepth";
+            this.deskDepth.Size = new System.Drawing.Size(120, 20);
+            this.deskDepth.TabIndex = 2;
+            // 
+            // numberOFDrawers
+            // 
+            this.numberOFDrawers.Location = new System.Drawing.Point(106, 19);
+            this.numberOFDrawers.Name = "numberOFDrawers";
+            this.numberOFDrawers.Size = new System.Drawing.Size(120, 20);
+            this.numberOFDrawers.TabIndex = 3;
+            // 
+            // materialLaminate
+            // 
+            this.materialLaminate.AutoSize = true;
+            this.materialLaminate.Location = new System.Drawing.Point(16, 30);
+            this.materialLaminate.Name = "materialLaminate";
+            this.materialLaminate.Size = new System.Drawing.Size(76, 17);
+            this.materialLaminate.TabIndex = 4;
+            this.materialLaminate.TabStop = true;
+            this.materialLaminate.Text = "Laminate";
+            this.materialLaminate.UseVisualStyleBackColor = true;
+            // 
+            // materialOak
+            // 
+            this.materialOak.AutoSize = true;
+            this.materialOak.Location = new System.Drawing.Point(16, 53);
+            this.materialOak.Name = "materialOak";
+            this.materialOak.Size = new System.Drawing.Size(48, 17);
+            this.materialOak.TabIndex = 5;
+            this.materialOak.TabStop = true;
+            this.materialOak.Text = "Oak";
+            this.materialOak.UseVisualStyleBackColor = true;
+            // 
+            // materialRosewood
+            // 
+            this.materialRosewood.AutoSize = true;
+            this.materialRosewood.Location = new System.Drawing.Point(16, 76);
+            this.materialRosewood.Name = "materialRosewood";
+            this.materialRosewood.Size = new System.Drawing.Size(84, 17);
+            this.materialRosewood.TabIndex = 6;
+            this.materialRosewood.TabStop = true;
+            this.materialRosewood.Text = "Rosewood";
+            this.materialRosewood.UseVisualStyleBackColor = true;
+            // 
+            // materialVeneer
+            // 
+            this.materialVeneer.AutoSize = true;
+            this.materialVeneer.Location = new System.Drawing.Point(16, 99);
+            this.materialVeneer.Name = "materialVeneer";
+            this.materialVeneer.Size = new System.Drawing.Size(65, 17);
+            this.materialVeneer.TabIndex = 7;
+            this.materialVeneer.TabStop = true;
+            this.materialVeneer.Text = "Veneer";
+            this.materialVeneer.UseVisualStyleBackColor = true;
+            // 
+            // materialPine
+            // 
+            this.materialPine.AutoSize = true;
+            this.materialPine.Location = new System.Drawing.Point(16, 122);
+            this.materialPine.Name = "materialPine";
+            this.materialPine.Size = new System.Drawing.Size(50, 17);
+            this.materialPine.TabIndex = 8;
+            this.materialPine.TabStop = true;
+            this.materialPine.Text = "Pine";
+            this.materialPine.UseVisualStyleBackColor = true;
+            // 
+            // rushNone
+            // 
+            this.rushNone.AutoSize = true;
+            this.rushNone.Location = new System.Drawing.Point(17, 24);
+            this.rushNone.Name = "rushNone";
+            this.rushNone.Size = new System.Drawing.Size(126, 17);
+            this.rushNone.TabIndex = 9;
+            this.rushNone.TabStop = true;
+            this.rushNone.Text = "No Rush (14 Day)";
+            this.rushNone.UseVisualStyleBackColor = true;
+            // 
+            // rush3Day
+            // 
+            this.rush3Day.AutoSize = true;
+            this.rush3Day.Location = new System.Drawing.Point(17, 47);
+            this.rush3Day.Name = "rush3Day";
+            this.rush3Day.Size = new System.Drawing.Size(99, 17);
+            this.rush3Day.TabIndex = 10;
+            this.rush3Day.TabStop = true;
+            this.rush3Day.Text = "Rush - 3 Day";
+            this.rush3Day.UseVisualStyleBackColor = true;
+            // 
+            // rush5Day
+            // 
+            this.rush5Day.AutoSize = true;
+            this.rush5Day.Location = new System.Drawing.Point(17, 72);
+            this.rush5Day.Name = "rush5Day";
+            this.rush5Day.Size = new System.Drawing.Size(99, 17);
+            this.rush5Day.TabIndex = 11;
+            this.rush5Day.TabStop = true;
+            this.rush5Day.Text = "Rush - 5 Day";
+            this.rush5Day.UseVisualStyleBackColor = true;
+            // 
+            // rush7Day
+            // 
+            this.rush7Day.AutoSize = true;
+            this.rush7Day.Location = new System.Drawing.Point(17, 98);
+            this.rush7Day.Name = "rush7Day";
+            this.rush7Day.Size = new System.Drawing.Size(99, 17);
+            this.rush7Day.TabIndex = 12;
+            this.rush7Day.TabStop = true;
+            this.rush7Day.Text = "Rush - 7 Day";
+            this.rush7Day.UseVisualStyleBackColor = true;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(319, 266);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.TabIndex = 13;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(173, 266);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 14;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // customerName
+            // 
+            this.customerName.AutoSize = true;
+            this.customerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerName.Location = new System.Drawing.Point(30, 27);
+            this.customerName.Name = "customerName";
+            this.customerName.Size = new System.Drawing.Size(99, 13);
+            this.customerName.TabIndex = 15;
+            this.customerName.Text = "Customer Name:";
+            // 
+            // groupDeskDimensions
+            // 
+            this.groupDeskDimensions.Controls.Add(this.desktopDepth);
+            this.groupDeskDimensions.Controls.Add(this.desktopWidth);
+            this.groupDeskDimensions.Controls.Add(this.deskWidth);
+            this.groupDeskDimensions.Controls.Add(this.deskDepth);
+            this.groupDeskDimensions.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupDeskDimensions.Location = new System.Drawing.Point(22, 74);
+            this.groupDeskDimensions.Name = "groupDeskDimensions";
+            this.groupDeskDimensions.Size = new System.Drawing.Size(247, 110);
+            this.groupDeskDimensions.TabIndex = 16;
+            this.groupDeskDimensions.TabStop = false;
+            this.groupDeskDimensions.Text = "Desktop Dimensions";
+            // 
+            // desktopDepth
+            // 
+            this.desktopDepth.AutoSize = true;
+            this.desktopDepth.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.desktopDepth.Location = new System.Drawing.Point(19, 78);
+            this.desktopDepth.Name = "desktopDepth";
+            this.desktopDepth.Size = new System.Drawing.Size(82, 13);
+            this.desktopDepth.TabIndex = 4;
+            this.desktopDepth.Text = "Desktop Depth:";
+            // 
+            // desktopWidth
+            // 
+            this.desktopWidth.AutoSize = true;
+            this.desktopWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.desktopWidth.Location = new System.Drawing.Point(19, 32);
+            this.desktopWidth.Name = "desktopWidth";
+            this.desktopWidth.Size = new System.Drawing.Size(81, 13);
+            this.desktopWidth.TabIndex = 3;
+            this.desktopWidth.Text = "Desktop Width:";
+            // 
+            // groupNumDrawers
+            // 
+            this.groupNumDrawers.Controls.Add(this.numDrawers);
+            this.groupNumDrawers.Controls.Add(this.numberOFDrawers);
+            this.groupNumDrawers.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupNumDrawers.Location = new System.Drawing.Point(22, 191);
+            this.groupNumDrawers.Name = "groupNumDrawers";
+            this.groupNumDrawers.Size = new System.Drawing.Size(247, 56);
+            this.groupNumDrawers.TabIndex = 17;
+            this.groupNumDrawers.TabStop = false;
+            this.groupNumDrawers.Text = "Number of Drawers";
+            // 
+            // numDrawers
+            // 
+            this.numDrawers.AutoSize = true;
+            this.numDrawers.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numDrawers.Location = new System.Drawing.Point(19, 21);
+            this.numDrawers.Name = "numDrawers";
+            this.numDrawers.Size = new System.Drawing.Size(62, 13);
+            this.numDrawers.TabIndex = 4;
+            this.numDrawers.Text = "(Enter 0 - 7)";
+            // 
+            // groupSurfaceMaterial
+            // 
+            this.groupSurfaceMaterial.Controls.Add(this.materialOak);
+            this.groupSurfaceMaterial.Controls.Add(this.materialLaminate);
+            this.groupSurfaceMaterial.Controls.Add(this.materialRosewood);
+            this.groupSurfaceMaterial.Controls.Add(this.materialVeneer);
+            this.groupSurfaceMaterial.Controls.Add(this.materialPine);
+            this.groupSurfaceMaterial.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupSurfaceMaterial.Location = new System.Drawing.Point(282, 74);
+            this.groupSurfaceMaterial.Name = "groupSurfaceMaterial";
+            this.groupSurfaceMaterial.Size = new System.Drawing.Size(126, 173);
+            this.groupSurfaceMaterial.TabIndex = 18;
+            this.groupSurfaceMaterial.TabStop = false;
+            this.groupSurfaceMaterial.Text = "Surface Material";
+            // 
+            // groupRush
+            // 
+            this.groupRush.Controls.Add(this.rushNone);
+            this.groupRush.Controls.Add(this.rush3Day);
+            this.groupRush.Controls.Add(this.rush5Day);
+            this.groupRush.Controls.Add(this.rush7Day);
+            this.groupRush.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupRush.Location = new System.Drawing.Point(423, 74);
+            this.groupRush.Name = "groupRush";
+            this.groupRush.Size = new System.Drawing.Size(149, 173);
+            this.groupRush.TabIndex = 19;
+            this.groupRush.TabStop = false;
+            this.groupRush.Text = "Shipping";
             // 
             // AddQuote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 311);
+            this.Controls.Add(this.groupRush);
+            this.Controls.Add(this.groupSurfaceMaterial);
+            this.Controls.Add(this.groupNumDrawers);
+            this.Controls.Add(this.groupDeskDimensions);
+            this.Controls.Add(this.customerName);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.clientName);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AddQuote";
-            this.Text = "AddQuote";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New Quote - MegaDesk";
+            ((System.ComponentModel.ISupportInitialize)(this.deskWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deskDepth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOFDrawers)).EndInit();
+            this.groupDeskDimensions.ResumeLayout(false);
+            this.groupDeskDimensions.PerformLayout();
+            this.groupNumDrawers.ResumeLayout(false);
+            this.groupNumDrawers.PerformLayout();
+            this.groupSurfaceMaterial.ResumeLayout(false);
+            this.groupSurfaceMaterial.PerformLayout();
+            this.groupRush.ResumeLayout(false);
+            this.groupRush.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox clientName;
+        private System.Windows.Forms.NumericUpDown deskWidth;
+        private System.Windows.Forms.NumericUpDown deskDepth;
+        private System.Windows.Forms.NumericUpDown numberOFDrawers;
+        private System.Windows.Forms.RadioButton materialLaminate;
+        private System.Windows.Forms.RadioButton materialOak;
+        private System.Windows.Forms.RadioButton materialRosewood;
+        private System.Windows.Forms.RadioButton materialVeneer;
+        private System.Windows.Forms.RadioButton materialPine;
+        private System.Windows.Forms.RadioButton rushNone;
+        private System.Windows.Forms.RadioButton rush3Day;
+        private System.Windows.Forms.RadioButton rush5Day;
+        private System.Windows.Forms.RadioButton rush7Day;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Label customerName;
+        private System.Windows.Forms.GroupBox groupDeskDimensions;
+        private System.Windows.Forms.Label desktopDepth;
+        private System.Windows.Forms.Label desktopWidth;
+        private System.Windows.Forms.GroupBox groupNumDrawers;
+        private System.Windows.Forms.Label numDrawers;
+        private System.Windows.Forms.GroupBox groupSurfaceMaterial;
+        private System.Windows.Forms.GroupBox groupRush;
     }
 }
--- MegDesk-3-JeremyCherriman/Program.cs
+++ MegDesk-3-JeremyCherriman/Program.cs
@@ -19,16 +19,6 @@ namespace MegDesk_3_JeremyCherriman
             Application.Run(new MainMenu());
         }
 
-        
     }
 
-    private void openAddQuoteButton_Click(object sender, EventArgs e)
-    {
-
-        AddQuote addNewQuoteForm = new AddQuote();
-        addNewQuoteForm.Tag = this;
-        addNewQuoteForm.Show(this);
-        Hide();
-
-    }
 }
