e941ae33332ea4441991062f8c90ecf968683d67
Jeremy Cherriman <32113470+JNCherriman@users.noreply.github.com>
2019-01-26 20:12:18 -0500

    Added buttons, button text, and Megadesk logo. Resized window and positioned buttons/logo for design.

--- MegDesk-3-JeremyCherriman/MainMenu.Designer.cs
+++ MegDesk-3-JeremyCherriman/MainMenu.Designer.cs
@@ -28,21 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.addNewQuoteBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.MegaDeskLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MegaDeskLogo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // addNewQuoteBtn
+            // 
+            this.addNewQuoteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addNewQuoteBtn.Location = new System.Drawing.Point(51, 37);
+            this.addNewQuoteBtn.Name = "addNewQuoteBtn";
+            this.addNewQuoteBtn.Size = new System.Drawing.Size(150, 50);
+            this.addNewQuoteBtn.TabIndex = 0;
+            this.addNewQuoteBtn.Text = "Add New Quote";
+            this.addNewQuoteBtn.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(51, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "View Quotes";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(51, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Search Quotes";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(51, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // MegaDeskLogo
+            // 
+            this.MegaDeskLogo.Image = ((System.Drawing.Image)(resources.GetObject("MegaDeskLogo.Image")));
+            this.MegaDeskLogo.Location = new System.Drawing.Point(284, 37);
+            this.MegaDeskLogo.Name = "MegaDeskLogo";
+            this.MegaDeskLogo.Size = new System.Drawing.Size(225, 225);
+            this.MegaDeskLogo.TabIndex = 4;
+            this.MegaDeskLogo.TabStop = false;
             // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 311);
+            this.Controls.Add(this.MegaDeskLogo);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.addNewQuoteBtn);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MegaDesk - Main Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.MegaDeskLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button addNewQuoteBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox MegaDeskLogo;
     }
 }
 
